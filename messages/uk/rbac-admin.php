c1aa9647c187dca86f65faa7a7fee615c9398feb
Andrii Morozov <morozov@ztu.edu.ua>
2024-01-02 20:16:07 +0200

    Поточні правки

--- messages/uk/rbac-admin.php
+++ messages/uk/rbac-admin.php
@@ -17,7 +17,7 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Assignments' => 'Призначення',
+    'Assignments' => 'Призначення ролей',
     'Users' => 'Користувачі',
     'User' => 'Користувач',
     'Available' => 'Доступно',
